cef1513564fc2c5a2fa61476630cb91b31730f1b
LuizChristani <166182040+LuizChristani@users.noreply.github.com>
2025-05-14 21:58:44 -0300

    Fix: Ajustado para ser reutilizado
    Foi refeito para o layout ficar como componentes

---
+++ frontend/app/estoque/movimentacao/saida.tsx
@@ -0,0 +1,9 @@
+import LayoutNota from "./components/LayoutNota";
+
+export default function Saida() {
+    return (
+        <div className=" bg-white text-black">
+            <LayoutNota />
+        </div>
+    );
+}
